a340164d97369be40a2d3434d8fc711be40a72f2
SzalkaiOli <onlq05@inf.elte.hu>
2023-04-05 15:57:45 +0200

    final methods inplemented

---
+++ app/src/main/java/hu/oliver/todolist/RecyclerItemTouchHelper.java
@@ -0,0 +1,102 @@
+package hu.oliver.todolist;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import hu.oliver.myapplication.R;
+import hu.oliver.todolist.Adapter.ToDoAdapter;
+
+public class RecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback {
+    private ToDoAdapter adapter;
+
+    public RecyclerItemTouchHelper(ToDoAdapter adapter) {
+        super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+        this.adapter = adapter;
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder,
+                          @NonNull RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+        final int position = viewHolder.getAdapterPosition();
+
+        if (direction == ItemTouchHelper.LEFT) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(adapter.getContext());
+            builder.setTitle(R.string.task_deletion_title);
+            builder.setMessage(R.string.task_deletion_msg);
+
+            builder.setPositiveButton(R.string.task_deletion_pos_msg, (dialogInterface, i)
+                    -> adapter.deleteItem(position));
+
+            builder.setNegativeButton(android.R.string.cancel, (dialogInterface, i)
+                    -> adapter.notifyItemChanged(viewHolder.getAdapterPosition()));
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        } else {
+            adapter.editItem(position);
+        }
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                            float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+        Drawable icon;
+        ColorDrawable background;
+
+        View itemView = viewHolder.itemView;
+        int backgroundCornerOffset = 20;
+
+        if (dX > 0) {
+            icon = ContextCompat.getDrawable(adapter.getContext(), R.drawable.baseline_edit);
+            background = new ColorDrawable(ContextCompat.getColor(adapter.getContext(), R.color.dark_slate_gray));
+        } else {
+            icon = ContextCompat.getDrawable(adapter.getContext(), R.drawable.baseline_delete);
+            background = new ColorDrawable(Color.RED);
+        }
+
+        assert icon != null;
+        int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconBottom = iconTop + icon.getIntrinsicHeight();
+
+        if (dX > 0) {           // Swiping to the right
+            int iconLeft = itemView.getLeft() + iconMargin;
+            int iconRight = itemView.getLeft() + iconMargin + icon.getIntrinsicWidth();
+            icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+            background.setBounds(itemView.getLeft(), itemView.getTop(),
+                    itemView.getLeft() + ((int) dX) + backgroundCornerOffset, itemView.getBottom());
+        } else if (dX < 0) {    // Swiping to the left
+            int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
+            int iconRight = itemView.getRight() - iconMargin;
+            icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+            background.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffset,
+                    itemView.getTop(), itemView.getRight(), itemView.getBottom());
+        } else {                // view is unSwiped
+            background.setBounds(0, 0, 0, 0);
+        }
+
+        background.draw(c);
+        icon.draw(c);
+    }
+}
